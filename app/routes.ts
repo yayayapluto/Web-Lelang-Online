eaf35c99d7f9c2f5633f578f2a8903403cd7b42c
yyypluto <mfarrasashshiddiqi@gmail.com>
2025-08-03 11:39:19 +0700

    chore: layout setup

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,7 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import {type RouteConfig, index, layout, route} from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    layout("components/layouts/dashboardLayout.tsx", [
+        index("routes/dashboard.tsx")
+    ])
+] satisfies RouteConfig;
