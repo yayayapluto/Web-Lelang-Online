ab5e32812540e0cd53818028748896a13e98b743
yyypluto <mfarrasashshiddiqi@gmail.com>
2025-08-03 18:31:26 +0700

    chore: components init

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,13 @@
-import {type RouteConfig, index, layout, route} from "@react-router/dev/routes";
+import {type RouteConfig, index, layout, route, prefix} from "@react-router/dev/routes";
 
 export default [
     layout("components/layouts/dashboardLayout.tsx", [
-        index("routes/dashboard.tsx")
+        index("routes/dashboard.tsx"),
+        ...prefix("modul-negara", [
+            index("components/pages/countries/index.tsx"),
+            // route("/add", "components/pages/countries/create-form.tsx"),
+            // route("/:id", "components/pages/countries/detail.tsx"),
+            // route("/:id/edit", "components/pages/countries/update-form.tsx"),
+        ])
     ])
 ] satisfies RouteConfig;
