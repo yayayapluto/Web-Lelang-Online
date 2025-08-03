335430a6d0328c52d85fb1a511a601b4fa141744
yyypluto <mfarrasashshiddiqi@gmail.com>
2025-08-03 18:55:41 +0700

    feat: province index

--- app/routes.ts
+++ app/routes.ts
@@ -8,6 +8,12 @@ export default [
             // route("/add", "components/pages/countries/create-form.tsx"),
             // route("/:id", "components/pages/countries/detail.tsx"),
             // route("/:id/edit", "components/pages/countries/update-form.tsx"),
+        ]),
+        ...prefix("modul-provinsi", [
+            index("components/pages/provinces/index.tsx"),
+            // route("/add", "components/pages/countries/create-form.tsx"),
+            // route("/:id", "components/pages/countries/detail.tsx"),
+            // route("/:id/edit", "components/pages/countries/update-form.tsx"),
         ])
     ])
 ] satisfies RouteConfig;
