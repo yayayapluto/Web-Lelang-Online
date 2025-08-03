6d12fbbb20be53d54f0b1b2618b103de03496f44
yyypluto <mfarrasashshiddiqi@gmail.com>
2025-08-04 05:28:16 +0700

    feat: index modul penyelenggara

--- app/routes.ts
+++ app/routes.ts
@@ -68,6 +68,12 @@ export default [
             // route("/add", "components/pages/countries/create-form.tsx"),
             // route("/:id", "components/pages/countries/detail.tsx"),
             // route("/:id/edit", "components/pages/countries/update-form.tsx"),
+        ]),
+        ...prefix("modul-penyelenggara", [
+            index("components/pages/organizers/index.tsx"),
+            // route("/add", "components/pages/countries/create-form.tsx"),
+            // route("/:id", "components/pages/countries/detail.tsx"),
+            // route("/:id/edit", "components/pages/countries/update-form.tsx"),
         ])
     ])
 ] satisfies RouteConfig;
