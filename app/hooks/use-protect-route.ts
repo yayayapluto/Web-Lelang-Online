75ad3de0154dbcdfd2b18e02c5169bb4e6e3241c
yyypluto <mfarrasashshiddiqi@gmail.com>
2025-08-07 13:06:21 +0700

    some fix

--- app/hooks/use-protect-route.ts
+++ app/hooks/use-protect-route.ts
@@ -25,7 +25,7 @@ export const useProtectRoute = () => {
     }, []);
 
     useEffect(() => {
-        if (!isLoading && result && result.role === "admin") {
+        if (!isLoading && result) {
             return;
         }
 
